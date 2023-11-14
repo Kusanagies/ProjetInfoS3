ae61d31093fcd049556b268d5519561a9e60a6f9
Kusanagies <Sylvainhuang3@gmail.com>
2023-11-14 11:41:08 +0100

    Ajout du fichier flask_c_app

---
+++ flask_c_app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Compile and run the C file
+    compile_and_run_c()
+    
+    # Read the output from the C program
+    result = read_c_output()
+
+    return render_template('index.html', result=result)
+
+def compile_and_run_c():
+    # Use subprocess to compile the C file
+    subprocess.run(['gcc', 'a.c', '-o', 'output_file'])
+
+    # Run the compiled executable
+    subprocess.run(['./output_file'])
+
+def read_c_output():
+    # Read the output from the C program
+    result = subprocess.check_output(['./output_file'], text=True)
+    return result
+
+if __name__ == '__main__':
+    app.run(debug=True)
