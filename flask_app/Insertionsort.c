2fc7cd9ebd6b4efd209827ebb1c3b97d912942e5
Kusanagies <Sylvainhuang3@gmail.com>
2023-12-26 19:23:28 +0100

    Modification des fichiers html dans templates et modification du insertionsort.c pour qu'il marche

--- flask_app/Insertionsort.c
+++ flask_app/Insertionsort.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
 void insertionSort(int arr[], int n, FILE *outputFile) {
     int i, key, j;
     for (i = 1; i < n; i++) {
         key = arr[i];
         j = i - 1;
 
-        /* Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position */
+        // Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
@@ -18,22 +17,22 @@ void insertionSort(int arr[], int n, FILE *outputFile) {
         for (int k = 0; k < n; k++) {
             fprintf(outputFile, "%d", arr[k]);
 
-            // Print a semicolon after each number, except the last one
+            // Print a semicolon after each number, but not after the last one
             if (k < n - 1) {
                 fprintf(outputFile, ";");
             }
         }
-        fprintf(outputFile, "\n");
+        fprintf(outputFile, ";\n");  // Add a semicolon and a newline after each iteration
     }
 }
 
-//Le main va nous donner un fichier csv pour pouvoir donner les données des itérations aux fichiers python
+// Le main va nous donner un fichier csv pour pouvoir donner les données des itérations aux fichiers python
 int main() {
     int taille = 100;
     int arr[100];
-    int i,tmp;
-    for(i = 0;i < taille;i++){
-        tmp = rand()%100;
+    int i, tmp;
+    for (i = 0; i < taille; i++) {
+        tmp = rand() % 100;
         arr[i] = tmp;
     }
     FILE *outputFile = fopen("output.csv", "w");
@@ -43,15 +42,16 @@ int main() {
     }
 
     for (int i = 0; i < taille; i++) {
-        fprintf(outputFile, "%d;", arr[i]);
+        fprintf(outputFile, "%d", arr[i]);
+
+        // Print a semicolon after each number, but not after the last one
+        if (i < taille - 1) {
+            fprintf(outputFile, ";");
+        }
     }
-    fprintf(outputFile, "\n");
+    fprintf(outputFile, ";\n");  // Add a semicolon and a newline after the initial array
 
     insertionSort(arr, taille, outputFile);
-
-    for (int i = 0; i < taille; i++) {
-        fprintf(outputFile, "%d;", arr[i]);
-    }
 
     fclose(outputFile);
 
