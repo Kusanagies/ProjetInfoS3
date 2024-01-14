ea8b30d06c500e144aeac6a0cc6e805c3fcdcded
Kusanagies <Sylvainhuang3@gmail.com>
2024-01-14 13:27:08 +0100

    Ajout d'identification des erreurs lors de la compilation et l'éxécution

--- flask_app5/app.py
+++ flask_app5/app.py
@@ -4,6 +4,7 @@ from matplotlib.animation import FuncAnimation
 import csv
 import subprocess
 from flask import Flask, render_template, request
+import time
 
 app = Flask(__name__)
 
@@ -27,34 +28,146 @@ if line_count != -1:
     print(f"Le fichier '{file_path}' contient {line_count} lignes.")
     
 def compile_and_run_c_bubblesort(array_size):
-    # Use subprocess to compile the C file
-    subprocess.run(['gcc', 'bubblesort.c', '-o', 'output_file'])
-    # Run the compiled executable with the array size as a command-line argument
-    subprocess.run(['./output_file', str(array_size)])
+    start_time = time.time
+    try: 
+        # Use subprocess to compile the C file
+        compile_result = subprocess.run(['gcc', 'bubblesort.c', '-o', 'output_file'])
+        if compile_result.returncode == 0: 
+            # Run the compiled executable with the array size as a command-line argument
+            run_result = subprocess.run(['./output_file', str(array_size)])
+            execution_time = time.time() - start_time
+            if run_result.returncode == 0:
+                print(f"Bubble Compilation and execution successful. Time taken:{execution_time:.4f}seconds\n{run_result.stdout}")
+            else: 
+                print(f"Bubble Execution error: {run_result.stderr}")
+        else: 
+            print(f"Bubble Compilation error: {compile_result.stderr}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    finally:
+        subprocess.run(['rm','output_file'])
+        
+    
 
 def compile_and_run_c_Insertionsort(array_size):
-    subprocess.run(['gcc', 'Insertionsort.c', '-o', 'output_file']) 
-    subprocess.run(['./output_file', str(array_size)])
+    start_time = time.time
+    try: 
+        # Use subprocess to compile the C file
+        compile_result = subprocess.run(['gcc', 'Insertionsort.c', '-o', 'output_file'])
+        if compile_result.returncode == 0: 
+            # Run the compiled executable with the array size as a command-line argument
+            run_result = subprocess.run(['./output_file', str(array_size)])
+            execution_time = time.time() - start_time
+            if run_result.returncode == 0:
+                print(f"Ins Compilation and execution successful. Time taken:{execution_time:.4f}seconds\n{run_result.stdout}")
+            else: 
+                print(f"Ins Execution error: {run_result.stderr}")
+        else: 
+            print(f"Ins Compilation error: {compile_result.stderr}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    finally:
+        subprocess.run(['rm','output_file'])
 
 def compile_and_run_c_Selectionsort(array_size):
-    subprocess.run(['gcc', 'selectionsort.c', '-o', 'output_file'])
-    subprocess.run(['./output_file', str(array_size)])
+    start_time = time.time
+    try: 
+        # Use subprocess to compile the C file
+        compile_result = subprocess.run(['gcc', 'selectionsort.c', '-o', 'output_file'])
+        if compile_result.returncode == 0: 
+            # Run the compiled executable with the array size as a command-line argument
+            run_result = subprocess.run(['./output_file', str(array_size)])
+            execution_time = time.time() - start_time
+            if run_result.returncode == 0:
+                print(f"Select Compilation and execution successful. Time taken:{execution_time:.4f}seconds\n{run_result.stdout}")
+            else: 
+                print(f"Select Execution error: {run_result.stderr}")
+        else: 
+            print(f"Select Compilation error: {compile_result.stderr}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    finally:
+        subprocess.run(['rm','output_file'])
 
 def compile_and_run_c_Gnomesort(array_size):
-    subprocess.run(['gcc', 'Gnomesort.c', '-o', 'output_file'])
-    subprocess.run(['./output_file', str(array_size)])
+    start_time = time.time
+    try: 
+        # Use subprocess to compile the C file
+        compile_result = subprocess.run(['gcc', 'Gnomesort.c', '-o', 'output_file'])
+        if compile_result.returncode == 0: 
+            # Run the compiled executable with the array size as a command-line argument
+            run_result = subprocess.run(['./output_file', str(array_size)])
+            execution_time = time.time() - start_time
+            if run_result.returncode == 0:
+                print(f"Gnome Compilation and execution successful. Time taken:{execution_time:.4f}seconds\n{run_result.stdout}")
+            else: 
+                print(f"Gnome Execution error: {run_result.stderr}")
+        else: 
+            print(f"Gnome Compilation error: {compile_result.stderr}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    finally:
+        subprocess.run(['rm','output_file'])
 
 def compile_and_run_c_Bogosort(array_size):
-    subprocess.run(['gcc', 'bogosort.c', '-o', 'output_file'])
-    subprocess.run(['./output_file', str(array_size)])
+    start_time = time.time
+    try: 
+        # Use subprocess to compile the C file
+        compile_result = subprocess.run(['gcc', 'bogosort.c', '-o', 'output_file'])
+        if compile_result.returncode == 0: 
+            # Run the compiled executable with the array size as a command-line argument
+            run_result = subprocess.run(['./output_file', str(array_size)])
+            execution_time = time.time() - start_time
+            if run_result.returncode == 0:
+                print(f"Bogo Compilation and execution successful. Time taken:{execution_time:.4f}seconds\n{run_result.stdout}")
+            else: 
+                print(f"Bogo Execution error: {run_result.stderr}")
+        else: 
+            print(f"Bogo Compilation error: {compile_result.stderr}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    finally:
+        subprocess.run(['rm','output_file'])
 
 def compile_and_run_c_Naturalsort(array_size):
-    subprocess.run(['gcc', 'Naturalsort.c', '-o', 'output_file'])
-    subprocess.run(['./output_file', str(array_size)])
+    start_time = time.time
+    try: 
+        # Use subprocess to compile the C file
+        compile_result = subprocess.run(['gcc', 'Naturalsort.c', '-o', 'output_file'])
+        if compile_result.returncode == 0: 
+            # Run the compiled executable with the array size as a command-line argument
+            run_result = subprocess.run(['./output_file', str(array_size)])
+            execution_time = time.time() - start_time
+            if run_result.returncode == 0:
+                print(f"Natural Compilation and execution successful. Time taken:{execution_time:.4f}seconds\n{run_result.stdout}")
+            else: 
+                print(f"Natural Execution error: {run_result.stderr}")
+        else: 
+            print(f"Natural Compilation error: {compile_result.stderr}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    finally:
+        subprocess.run(['rm','output_file'])
 
 def compile_and_run_c_trirapide(array_size):
-    subprocess.run(['gcc', 'trirapide.c', '-o', 'output_file'])
-    subprocess.run(['./output_file', str(array_size)])  
+    start_time = time.time
+    try: 
+        # Use subprocess to compile the C file
+        compile_result = subprocess.run(['gcc', 'trirapide.c', '-o', 'output_file'])
+        if compile_result.returncode == 0: 
+            # Run the compiled executable with the array size as a command-line argument
+            run_result = subprocess.run(['./output_file', str(array_size)])
+            execution_time = time.time() - start_time
+            if run_result.returncode == 0:
+                print(f"Rapide Compilation and execution successful. Time taken:{execution_time:.4f}seconds\n{run_result.stdout}")
+            else: 
+                print(f"Rapide Execution error: {run_result.stderr}")
+        else: 
+            print(f"Rapide Compilation error: {compile_result.stderr}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    finally:
+        subprocess.run(['rm','output_file'])  
 
 def sorting_animation(data):
     plt.switch_backend('agg')  # Switch to non-interactive mode
